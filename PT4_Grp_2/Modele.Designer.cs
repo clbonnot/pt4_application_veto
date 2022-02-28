a4cb14f0aba0ff16bce0906198b8d3848c226a52
rbailhet <raphael.bailhet@etu.u-bordeaux.fr>
2022-02-28 11:33:00 +0100

    modele heritage ihm

---
+++ PT4_Grp_2/Modele.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PT4_Grp_2
+{
+	partial class Modele
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Deconnexion = new System.Windows.Forms.Button();
+			this.menuStrip3 = new System.Windows.Forms.MenuStrip();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+			this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.rôleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip3.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.menuStrip2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// Deconnexion
+			// 
+			this.Deconnexion.FlatAppearance.BorderSize = 0;
+			this.Deconnexion.Location = new System.Drawing.Point(692, 12);
+			this.Deconnexion.Name = "Deconnexion";
+			this.Deconnexion.Size = new System.Drawing.Size(81, 24);
+			this.Deconnexion.TabIndex = 7;
+			this.Deconnexion.Text = "Deconnexion";
+			this.Deconnexion.UseVisualStyleBackColor = true;
+			// 
+			// menuStrip3
+			// 
+			this.menuStrip3.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.menuStrip3.Dock = System.Windows.Forms.DockStyle.Left;
+			this.menuStrip3.GripMargin = new System.Windows.Forms.Padding(2, 5, 0, 2);
+			this.menuStrip3.ImageScalingSize = new System.Drawing.Size(16, 20);
+			this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+			this.menuStrip3.Location = new System.Drawing.Point(0, 48);
+			this.menuStrip3.Name = "menuStrip3";
+			this.menuStrip3.Size = new System.Drawing.Size(151, 402);
+			this.menuStrip3.TabIndex = 10;
+			this.menuStrip3.Text = "menuStrip3";
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(138, 19);
+			this.toolStripMenuItem1.Text = "                                          ";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.BackColor = System.Drawing.Color.DarkGreen;
+			this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 5, 0, 2);
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(16, 20);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rôleToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 8;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// menuStrip2
+			// 
+			this.menuStrip2.BackColor = System.Drawing.Color.DarkGreen;
+			this.menuStrip2.GripMargin = new System.Windows.Forms.Padding(2, 5, 0, 2);
+			this.menuStrip2.ImageScalingSize = new System.Drawing.Size(16, 20);
+			this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adminToolStripMenuItem});
+			this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip2.Name = "menuStrip2";
+			this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip2.TabIndex = 9;
+			this.menuStrip2.Text = "menuStrip2";
+			// 
+			// adminToolStripMenuItem
+			// 
+			this.adminToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
+			this.adminToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+			this.adminToolStripMenuItem.Text = "Personne : ";
+			// 
+			// rôleToolStripMenuItem
+			// 
+			this.rôleToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.rôleToolStripMenuItem.Name = "rôleToolStripMenuItem";
+			this.rôleToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+			this.rôleToolStripMenuItem.Text = "Rôle : ";
+			// 
+			// Modele
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Deconnexion);
+			this.Controls.Add(this.menuStrip3);
+			this.Controls.Add(this.menuStrip1);
+			this.Controls.Add(this.menuStrip2);
+			this.Name = "Modele";
+			this.Text = "Modele";
+			this.menuStrip3.ResumeLayout(false);
+			this.menuStrip3.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.menuStrip2.ResumeLayout(false);
+			this.menuStrip2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button Deconnexion;
+		private System.Windows.Forms.MenuStrip menuStrip3;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem rôleToolStripMenuItem;
+		private System.Windows.Forms.MenuStrip menuStrip2;
+		private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
+	}
+}
