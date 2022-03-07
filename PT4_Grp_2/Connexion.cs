8eca8193a50b813c585d07cebb38eac0af76e7ad
marakotoaris <mathieu.rakotoarisoa@etu.u-bordeaux.fr>
2022-03-07 11:23:32 +0100

    connection test

--- PT4_Grp_2/Connexion.cs
+++ PT4_Grp_2/Connexion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,18 @@ namespace PT4_Grp_2
         public Connexion()
         {
             InitializeComponent();
+            Console.WriteLine("eho");
+            DB dTest = new DB("INFO-JOYEUX", "PT4_E2");
+            dTest.openConnection();
+            OleDbCommand cmd = new OleDbCommand("SELECT Code_Animal FROM Animal WHERE Code_Animal = 2", dTest.getDbConnection());
+            OleDbDataReader r = cmd.ExecuteReader();
+            
+            while (r.Read())
+            {
+                Console.WriteLine("eho 5 ");
+                Console.WriteLine(r.GetString(1));   
+            }
+            dTest.closeConnection();
         }
     }
 }
