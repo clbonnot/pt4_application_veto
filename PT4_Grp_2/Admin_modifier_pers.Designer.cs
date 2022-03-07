cd38e56c57dd2ba885f1c0bfa257944ff6ecbb2b
rbailhet <raphael.bailhet@etu.u-bordeaux.fr>
2022-03-07 11:56:54 +0100

    liens projet-forms

---
+++ PT4_Grp_2/Admin_modifier_pers.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace PT4_Grp_2
+{
+	partial class Admin_modifier_pers
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// Admin_modifier_pers
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(360, 642);
+			this.Name = "Admin_modifier_pers";
+			this.Text = "Admin_modifier_pers";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+	}
+}
