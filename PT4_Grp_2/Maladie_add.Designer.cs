c720f91ceb7321cf5e79c5f36e3ba27f5d4e7c2a
rbailhet <raphael.bailhet@etu.u-bordeaux.fr>
2022-03-18 11:49:52 +0100

    stats push prec, maladie

--- PT4_Grp_2/Maladie_add.Designer.cs
+++ PT4_Grp_2/Maladie_add.Designer.cs
@@ -37,30 +37,34 @@
 			this.progressBar1 = new System.Windows.Forms.ProgressBar();
 			this.progressBar2 = new System.Windows.Forms.ProgressBar();
 			this.label3 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
 			this.description = new System.Windows.Forms.Label();
 			this.name_medic = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// textBox4
 			// 
-			this.textBox4.Location = new System.Drawing.Point(131, 356);
+			this.textBox4.Location = new System.Drawing.Point(132, 328);
 			this.textBox4.Name = "textBox4";
 			this.textBox4.Size = new System.Drawing.Size(209, 20);
 			this.textBox4.TabIndex = 63;
 			// 
 			// textBox3
 			// 
-			this.textBox3.Location = new System.Drawing.Point(131, 285);
+			this.textBox3.Location = new System.Drawing.Point(132, 267);
 			this.textBox3.Name = "textBox3";
 			this.textBox3.Size = new System.Drawing.Size(209, 20);
 			this.textBox3.TabIndex = 62;
 			// 
 			// textBox2
 			// 
-			this.textBox2.Location = new System.Drawing.Point(131, 220);
+			this.textBox2.Location = new System.Drawing.Point(132, 202);
 			this.textBox2.Name = "textBox2";
 			this.textBox2.Size = new System.Drawing.Size(209, 20);
 			this.textBox2.TabIndex = 61;
@@ -119,16 +123,6 @@
 			this.label3.TabIndex = 55;
 			this.label3.Text = "Ajouter une maladie";
 			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(128, 265);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(62, 17);
-			this.label2.TabIndex = 54;
-			this.label2.Text = "Allergies";
-			// 
 			// label5
 			// 
 			this.label5.AutoSize = true;
@@ -139,21 +133,11 @@
 			this.label5.TabIndex = 53;
 			this.label5.Text = "Informations";
 			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(128, 186);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(56, 17);
-			this.label1.TabIndex = 52;
-			this.label1.Text = "Marque";
-			// 
 			// description
 			// 
 			this.description.AutoSize = true;
 			this.description.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.description.Location = new System.Drawing.Point(128, 326);
+			this.description.Location = new System.Drawing.Point(129, 308);
 			this.description.Name = "description";
 			this.description.Size = new System.Drawing.Size(79, 17);
 			this.description.TabIndex = 51;
@@ -169,11 +153,72 @@
 			this.name_medic.TabIndex = 50;
 			this.name_medic.Text = "Nom";
 			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(129, 433);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(83, 17);
+			this.label4.TabIndex = 68;
+			this.label4.Text = "Traitements";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(129, 372);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(140, 17);
+			this.label6.TabIndex = 67;
+			this.label6.Text = "Espèces concernées";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(129, 182);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(85, 17);
+			this.label7.TabIndex = 66;
+			this.label7.Text = "Propagation";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(129, 247);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(81, 17);
+			this.label8.TabIndex = 65;
+			this.label8.Text = "Symptômes";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(132, 392);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(209, 20);
+			this.textBox5.TabIndex = 69;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(132, 453);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(209, 20);
+			this.textBox6.TabIndex = 70;
+			// 
 			// Maladie_add
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(465, 574);
+			this.BackColor = System.Drawing.SystemColors.Info;
+			this.ClientSize = new System.Drawing.Size(464, 574);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label8);
 			this.Controls.Add(this.textBox4);
 			this.Controls.Add(this.textBox3);
 			this.Controls.Add(this.textBox2);
@@ -183,9 +228,7 @@
 			this.Controls.Add(this.progressBar1);
 			this.Controls.Add(this.progressBar2);
 			this.Controls.Add(this.label3);
-			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label5);
-			this.Controls.Add(this.label1);
 			this.Controls.Add(this.description);
 			this.Controls.Add(this.name_medic);
 			this.Name = "Maladie_add";
@@ -206,10 +249,14 @@
 		private System.Windows.Forms.ProgressBar progressBar1;
 		private System.Windows.Forms.ProgressBar progressBar2;
 		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label5;
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label description;
 		private System.Windows.Forms.Label name_medic;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
 	}
 }
