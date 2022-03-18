551d40730ea669e30a2a85a274d56f0b3c960176
rbailhet <raphael.bailhet@etu.u-bordeaux.fr>
2022-03-18 10:24:43 +0100

    correction stockdetails, userForm nav secretaire veto

---
+++ PT4_Grp_2/Control_secretaire.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PT4_Grp_2
+{
+	partial class Control_secretaire
+	{
+		/// <summary> 
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur de composants
+
+		/// <summary> 
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.stats = new System.Windows.Forms.Button();
+			this.client = new System.Windows.Forms.Button();
+			this.maladie = new System.Windows.Forms.Button();
+			this.stock = new System.Windows.Forms.Button();
+			this.Calendrier = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// stats
+			// 
+			this.stats.Location = new System.Drawing.Point(3, 135);
+			this.stats.Name = "stats";
+			this.stats.Size = new System.Drawing.Size(107, 27);
+			this.stats.TabIndex = 70;
+			this.stats.Text = "Statistiques";
+			this.stats.UseVisualStyleBackColor = true;
+			// 
+			// client
+			// 
+			this.client.Location = new System.Drawing.Point(3, 102);
+			this.client.Name = "client";
+			this.client.Size = new System.Drawing.Size(107, 27);
+			this.client.TabIndex = 69;
+			this.client.Text = "Clients";
+			this.client.UseVisualStyleBackColor = true;
+			// 
+			// maladie
+			// 
+			this.maladie.Enabled = false;
+			this.maladie.Location = new System.Drawing.Point(3, 69);
+			this.maladie.Name = "maladie";
+			this.maladie.Size = new System.Drawing.Size(107, 27);
+			this.maladie.TabIndex = 68;
+			this.maladie.Text = "Maladies";
+			this.maladie.UseVisualStyleBackColor = true;
+			// 
+			// stock
+			// 
+			this.stock.Location = new System.Drawing.Point(3, 36);
+			this.stock.Name = "stock";
+			this.stock.Size = new System.Drawing.Size(107, 27);
+			this.stock.TabIndex = 67;
+			this.stock.Text = "Stocks";
+			this.stock.UseVisualStyleBackColor = true;
+			// 
+			// Calendrier
+			// 
+			this.Calendrier.Location = new System.Drawing.Point(3, 3);
+			this.Calendrier.Name = "Calendrier";
+			this.Calendrier.Size = new System.Drawing.Size(107, 27);
+			this.Calendrier.TabIndex = 66;
+			this.Calendrier.Text = "Calendrier";
+			this.Calendrier.UseVisualStyleBackColor = true;
+			// 
+			// Control_secretaire
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.Controls.Add(this.stats);
+			this.Controls.Add(this.client);
+			this.Controls.Add(this.maladie);
+			this.Controls.Add(this.stock);
+			this.Controls.Add(this.Calendrier);
+			this.Name = "Control_secretaire";
+			this.Size = new System.Drawing.Size(114, 169);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button stats;
+		private System.Windows.Forms.Button client;
+		private System.Windows.Forms.Button maladie;
+		private System.Windows.Forms.Button stock;
+		private System.Windows.Forms.Button Calendrier;
+	}
+}
--- PT4_Grp_2/Stocks_detail.Designer.cs
+++ PT4_Grp_2/Stocks_detail.Designer.cs
@@ -39,6 +39,13 @@
 			this.validate_buy = new System.Windows.Forms.Button();
 			this.button1 = new System.Windows.Forms.Button();
 			this.back_page = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.listFournisseur = new System.Windows.Forms.ListBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.product_pic)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -75,7 +82,7 @@
 			// 
 			this.description.AutoSize = true;
 			this.description.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.description.Location = new System.Drawing.Point(182, 236);
+			this.description.Location = new System.Drawing.Point(182, 300);
 			this.description.Name = "description";
 			this.description.Size = new System.Drawing.Size(79, 17);
 			this.description.TabIndex = 14;
@@ -147,11 +154,86 @@
 			this.back_page.Text = "Retour";
 			this.back_page.UseVisualStyleBackColor = true;
 			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(193, 185);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(56, 17);
+			this.label1.TabIndex = 23;
+			this.label1.Text = "Marque";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(263, 114);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(16, 17);
+			this.label2.TabIndex = 24;
+			this.label2.Text = "0";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(280, 185);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(83, 17);
+			this.label3.TabIndex = 25;
+			this.label3.Text = "Fournisseur";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(409, 185);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(47, 17);
+			this.label4.TabIndex = 26;
+			this.label4.Text = "Prix /u";
+			// 
+			// listFournisseur
+			// 
+			this.listFournisseur.FormattingEnabled = true;
+			this.listFournisseur.Location = new System.Drawing.Point(283, 205);
+			this.listFournisseur.Name = "listFournisseur";
+			this.listFournisseur.Size = new System.Drawing.Size(80, 69);
+			this.listFournisseur.TabIndex = 27;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(182, 154);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(85, 17);
+			this.label5.TabIndex = 28;
+			this.label5.Text = "Informations";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(409, 217);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(24, 17);
+			this.label6.TabIndex = 29;
+			this.label6.Text = "3€";
+			// 
 			// Stocks_detail
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.listFournisseur);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.back_page);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.validate_buy);
@@ -176,6 +258,13 @@
 			this.Controls.SetChildIndex(this.validate_buy, 0);
 			this.Controls.SetChildIndex(this.button1, 0);
 			this.Controls.SetChildIndex(this.back_page, 0);
+			this.Controls.SetChildIndex(this.label1, 0);
+			this.Controls.SetChildIndex(this.label2, 0);
+			this.Controls.SetChildIndex(this.label3, 0);
+			this.Controls.SetChildIndex(this.label4, 0);
+			this.Controls.SetChildIndex(this.listFournisseur, 0);
+			this.Controls.SetChildIndex(this.label5, 0);
+			this.Controls.SetChildIndex(this.label6, 0);
 			((System.ComponentModel.ISupportInitialize)(this.product_pic)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -195,5 +284,12 @@
 		private System.Windows.Forms.Button validate_buy;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Button back_page;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ListBox listFournisseur;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
 	}
 }
