0d54399f13b787125090c3020e8b071386a08f34
Aïssa KAHLOUL <aissageiffel@outlook.com>
2022-03-21 16:35:17 +0100

    Création package spécifique aux test pour rendre le code propre

--- PT4_Grp_2/TestUnitaires/Test.cs
+++ PT4_Grp_2/TestUnitaires/Test.cs
@@ -6,48 +6,19 @@ using System.Data.OleDb;
 
 namespace PT4_Grp_2.TestUnitaires
 {
-    internal class Test
+
+    [TestClass]
+    public class TestUnitaires
     {
-        [TestClass]
-        public class TestUS2
+
+        OleDbConnection dbCon;
+        string ChaineBd = "Provider=SQLOLEDB;Data Source=INFO-JOYEUX;Initial Catalog=MusiquePT4_E2;Integrated Security=SSPI;";
+        public void InitConnexion()
         {
-            Accueil accueil = new Accueil();
-
-            OleDbConnection dbCon;
-            string ChaineBd = "Provider=SQLOLEDB;Data Source=INFO-JOYEUX;Initial Catalog=MusiquePT4_E2;Integrated Security=SSPI;";
-            List<Albums> albumAbo = new List<Albums>();
-            public void InitConnexion()
-            {
-                dbCon = new OleDbConnection(ChaineBd);
-                dbCon.Open();
-            }
-            [TestMethod]
-            public void TestList1EmpruntAbo()
-            {
-                InitConnexion();
-                bool same = true;
-                int codeAbo = 0;
-                int codeAlbumEmprunter = 515;
-                int codeAlbumsGet = 0;
-                string titreAlbums = "";
-                DateTime dateRetour = new DateTime();
-                Client_Inscription client = new Client_Inscription();
-                string login = "testUS2";
-                string mdp = "US2TestMDP";
-                string prenom = "US2TestPrenom";
-                string nom = "US2TestNom";
-                string nationalite = "France";
-                client.AddAbonnes(login, nationalite, nom, mdp, prenom);
-                Abonne_Accueil accueilAbo = new Abonne_Accueil(nom, prenom, login);
-                string getCode = "SELECT CODE_ABONNÉ FROM ABONNÉS WHERE LOGIN_ABONNÉ ='" + login + "'";
-                OleDbCommand cmdCode = new OleDbCommand(getCode, dbCon);
-                cmdCode.ExecuteNonQuery();
-                OleDbDataReader readCode = cmdCode.ExecuteReader();
-                while (readCode.Read())
-                {
-                    codeAbo = readCode.GetInt32(0);
-                }
-                readCode.Close();
-
-            }
+            dbCon = new OleDbConnection(ChaineBd);
+            dbCon.Open();
         }
+        // [TestMethod]
+
+    }
+}
