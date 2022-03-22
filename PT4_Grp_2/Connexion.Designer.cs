c8485a05b713ae9d7201707c31f3d412d6b02aec
t.halley@numericable.fr <t.halley@numericable.fr>
2022-03-22 15:21:31 +0100

    résolution merge

--- PT4_Grp_2/Connexion.Designer.cs
+++ PT4_Grp_2/Connexion.Designer.cs
@@ -28,109 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Connexion));
-			this.menuStrip2 = new System.Windows.Forms.MenuStrip();
-			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.pictureBox2 = new System.Windows.Forms.PictureBox();
-			this.identifiant = new System.Windows.Forms.TextBox();
-			this.pwd = new System.Windows.Forms.TextBox();
-			this.connect = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// menuStrip2
-			// 
-			this.menuStrip2.BackColor = System.Drawing.Color.DarkGreen;
-			this.menuStrip2.GripMargin = new System.Windows.Forms.Padding(2, 5, 0, 2);
-			this.menuStrip2.ImageScalingSize = new System.Drawing.Size(16, 20);
-			this.menuStrip2.Location = new System.Drawing.Point(0, 0);
-			this.menuStrip2.Name = "menuStrip2";
-			this.menuStrip2.Size = new System.Drawing.Size(867, 24);
-			this.menuStrip2.TabIndex = 6;
-			this.menuStrip2.Text = "menuStrip2";
-			// 
-			// menuStrip1
-			// 
-			this.menuStrip1.BackColor = System.Drawing.Color.DarkGreen;
-			this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 5, 0, 2);
-			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(16, 20);
-			this.menuStrip1.Location = new System.Drawing.Point(0, 24);
-			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(867, 24);
-			this.menuStrip1.TabIndex = 7;
-			this.menuStrip1.Text = "menuStrip1";
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-			this.pictureBox1.Location = new System.Drawing.Point(12, 61);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(843, 536);
-			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox1.TabIndex = 8;
-			this.pictureBox1.TabStop = false;
-			// 
-			// pictureBox2
-			// 
-			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-			this.pictureBox2.Location = new System.Drawing.Point(500, 241);
-			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.Size = new System.Drawing.Size(138, 173);
-			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pictureBox2.TabIndex = 9;
-			this.pictureBox2.TabStop = false;
-			// 
-			// identifiant
-			// 
-			this.identifiant.BackColor = System.Drawing.SystemColors.Info;
-			this.identifiant.Location = new System.Drawing.Point(517, 299);
-			this.identifiant.Name = "identifiant";
-			this.identifiant.Size = new System.Drawing.Size(100, 20);
-			this.identifiant.TabIndex = 10;
-			this.identifiant.Text = "Identifiant";
-			this.identifiant.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-			// 
-			// pwd
-			// 
-			this.pwd.BackColor = System.Drawing.SystemColors.Info;
-			this.pwd.Location = new System.Drawing.Point(517, 334);
-			this.pwd.Name = "pwd";
-			this.pwd.Size = new System.Drawing.Size(100, 20);
-			this.pwd.TabIndex = 11;
-			this.pwd.Text = "Mot de passe";
-			this.pwd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-			// 
-			// connect
-			// 
-			this.connect.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-			this.connect.Location = new System.Drawing.Point(531, 374);
-			this.connect.Name = "connect";
-			this.connect.Size = new System.Drawing.Size(75, 23);
-			this.connect.TabIndex = 12;
-			this.connect.Text = "Connexion";
-			this.connect.UseVisualStyleBackColor = true;
-			// 
-			// Connexion
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.Info;
-			this.ClientSize = new System.Drawing.Size(867, 609);
-			this.Controls.Add(this.connect);
-			this.Controls.Add(this.pwd);
-			this.Controls.Add(this.identifiant);
-			this.Controls.Add(this.pictureBox2);
-			this.Controls.Add(this.pictureBox1);
-			this.Controls.Add(this.menuStrip1);
-			this.Controls.Add(this.menuStrip2);
-			this.Name = "Connexion";
-			this.Text = "Connexion";
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Connexion));
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.identifiant = new System.Windows.Forms.TextBox();
+            this.pwd = new System.Windows.Forms.TextBox();
+            this.connect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.BackColor = System.Drawing.Color.DarkGreen;
+            this.menuStrip2.GripMargin = new System.Windows.Forms.Padding(2, 5, 0, 2);
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(16, 20);
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(867, 24);
+            this.menuStrip2.TabIndex = 6;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DarkGreen;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 5, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(16, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(867, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(843, 536);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(500, 241);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(138, 173);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // identifiant
+            // 
+            this.identifiant.BackColor = System.Drawing.SystemColors.Info;
+            this.identifiant.Location = new System.Drawing.Point(517, 299);
+            this.identifiant.Name = "identifiant";
+            this.identifiant.Size = new System.Drawing.Size(100, 20);
+            this.identifiant.TabIndex = 10;
+            this.identifiant.Text = "Identifiant";
+            this.identifiant.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pwd
+            // 
+            this.pwd.BackColor = System.Drawing.SystemColors.Info;
+            this.pwd.Location = new System.Drawing.Point(517, 334);
+            this.pwd.Name = "pwd";
+            this.pwd.Size = new System.Drawing.Size(100, 20);
+            this.pwd.TabIndex = 11;
+            this.pwd.Text = "Mot de passe";
+            this.pwd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // connect
+            // 
+            this.connect.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.connect.Location = new System.Drawing.Point(531, 374);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(75, 23);
+            this.connect.TabIndex = 12;
+            this.connect.Text = "Connexion";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // Connexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(867, 609);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.pwd);
+            this.Controls.Add(this.identifiant);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Name = "Connexion";
+            this.Text = "Connexion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
