774c6b95872e5b70e49f43335c959c0de37adca0
rbailhet <raphael.bailhet@etu.u-bordeaux.fr>
2022-03-25 16:46:23 +0100

    clients / stocks

---
+++ PT4_Grp_2/Stocks_Fournisseur_add.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace PT4_Grp_2
+{
+	partial class Stocks_Fournisseur_add
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.name_product = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.add = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(80, 274);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(47, 17);
+			this.label4.TabIndex = 33;
+			this.label4.Text = "Prix /u";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(80, 213);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(37, 17);
+			this.label3.TabIndex = 32;
+			this.label3.Text = "Nom";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(80, 152);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(56, 17);
+			this.label1.TabIndex = 31;
+			this.label1.Text = "Marque";
+			// 
+			// name_product
+			// 
+			this.name_product.AutoSize = true;
+			this.name_product.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.name_product.Location = new System.Drawing.Point(78, 99);
+			this.name_product.Name = "name_product";
+			this.name_product.Size = new System.Drawing.Size(131, 26);
+			this.name_product.TabIndex = 30;
+			this.name_product.Text = "Nom produit";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(29, 30);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(212, 26);
+			this.label2.TabIndex = 36;
+			this.label2.Text = "Nouveau fournisseur";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(83, 172);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 38;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(83, 242);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 39;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(83, 303);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 40;
+			// 
+			// add
+			// 
+			this.add.BackColor = System.Drawing.Color.DarkSeaGreen;
+			this.add.Location = new System.Drawing.Point(156, 402);
+			this.add.Name = "add";
+			this.add.Size = new System.Drawing.Size(108, 36);
+			this.add.TabIndex = 42;
+			this.add.Text = "Ajouter";
+			this.add.UseVisualStyleBackColor = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(42, 402);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(108, 36);
+			this.button1.TabIndex = 41;
+			this.button1.Text = "Annuler";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// Stocks_Fournisseur_add
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Info;
+			this.ClientSize = new System.Drawing.Size(297, 450);
+			this.Controls.Add(this.add);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.name_product);
+			this.Name = "Stocks_Fournisseur_add";
+			this.Text = "Stocks_Fournisseur_add";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label name_product;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.Windows.Forms.Button add;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ PT4_Grp_2/Stocks_modifier.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace PT4_Grp_2
+{
+	partial class Stocks_modifier
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.description = new System.Windows.Forms.Label();
+			this.name_medic = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(79, 349);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(209, 20);
+			this.textBox4.TabIndex = 60;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(79, 278);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(209, 20);
+			this.textBox3.TabIndex = 59;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(79, 213);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(209, 20);
+			this.textBox2.TabIndex = 58;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(68, 109);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 57;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(76, 258);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(62, 17);
+			this.label2.TabIndex = 54;
+			this.label2.Text = "Allergies";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(65, 146);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(97, 17);
+			this.label5.TabIndex = 53;
+			this.label5.Text = "Informations";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(76, 179);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(56, 17);
+			this.label4.TabIndex = 52;
+			this.label4.Text = "Marque";
+			// 
+			// description
+			// 
+			this.description.AutoSize = true;
+			this.description.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.description.Location = new System.Drawing.Point(76, 319);
+			this.description.Name = "description";
+			this.description.Size = new System.Drawing.Size(79, 17);
+			this.description.TabIndex = 51;
+			this.description.Text = "Description";
+			// 
+			// name_medic
+			// 
+			this.name_medic.AutoSize = true;
+			this.name_medic.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.name_medic.Location = new System.Drawing.Point(63, 79);
+			this.name_medic.Name = "name_medic";
+			this.name_medic.Size = new System.Drawing.Size(59, 26);
+			this.name_medic.TabIndex = 50;
+			this.name_medic.Text = "Nom";
+			// 
+			// Stocks_modifier
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(372, 646);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.description);
+			this.Controls.Add(this.name_medic);
+			this.Name = "Stocks_modifier";
+			this.Text = "Stocks_modifier";
+			this.Controls.SetChildIndex(this.name_medic, 0);
+			this.Controls.SetChildIndex(this.description, 0);
+			this.Controls.SetChildIndex(this.label4, 0);
+			this.Controls.SetChildIndex(this.label5, 0);
+			this.Controls.SetChildIndex(this.label2, 0);
+			this.Controls.SetChildIndex(this.textBox1, 0);
+			this.Controls.SetChildIndex(this.textBox2, 0);
+			this.Controls.SetChildIndex(this.textBox3, 0);
+			this.Controls.SetChildIndex(this.textBox4, 0);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label description;
+		private System.Windows.Forms.Label name_medic;
+	}
+}
