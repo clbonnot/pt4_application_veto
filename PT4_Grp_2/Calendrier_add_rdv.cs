f0ba270806ebdd2703314de6e2553fb11f5281e4
Aïssa KAHLOUL <aissageiffel@outlook.com>
2022-03-25 17:34:47 +0100

    Remplissage des listbox calendrier

--- PT4_Grp_2/Calendrier_add_rdv.cs
+++ PT4_Grp_2/Calendrier_add_rdv.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,14 @@ namespace PT4_Grp_2
 {
 	public partial class Calendrier_add_rdv : Form
 	{
-		public Calendrier_add_rdv()
+		List<string> docs = new List<string>();
+		List<string> client = new List<string>();
+        static DB connexion = new DB("INFO-JOYEUX", "PT4_E2");
+        OleDbConnection dbCon = connexion.getDbConnection();
+        public Calendrier_add_rdv()
 		{
 			InitializeComponent();
+            refreshLists();
 		}
 
 		private void textBox1_TextChanged(object sender, EventArgs e)
@@ -26,5 +32,68 @@ namespace PT4_Grp_2
 		{
 
 		}
-	}
+
+        public void ConsultClient()
+        {
+            client.Clear();
+            listClient.Items.Clear();           
+            string sql = "select NOM,PRENOM from Personne " +
+                "left outer JOIN Personnel ON Personne.CODE_PERSONNE = Personnel.CODE_PERSONNEL " +
+                "WHERE Personnel.CODE_PERSONNEL is null ";
+
+
+            OleDbCommand cmdRead = new OleDbCommand(sql, dbCon);
+            OleDbDataReader reader = cmdRead.ExecuteReader();
+
+
+            while (reader.Read())
+            {
+                string nomprenom = reader.GetString(0).Trim() + reader.GetString(1).Trim();               
+                client.Add(nomprenom);
+            }
+
+            foreach (string element in client)
+            {
+                listClient.Items.Add(element);
+            }
+
+            reader.Close();
+        }
+
+        public void ConsultDocs()
+        {
+            docs.Clear();
+            listDoc.Items.Clear();
+            string sql = "select Personne.NOM,Personne.PRENOM from Personnel " +
+            "inner join Personne on Personnel.CODE_PERSONNE = Personne.CODE_PERSONNE";
+
+
+            OleDbCommand cmdRead = new OleDbCommand(sql, dbCon);
+            OleDbDataReader reader = cmdRead.ExecuteReader();
+
+
+            while (reader.Read())
+            {
+                string nomprenom = reader.GetString(0).Trim() + reader.GetString(1).Trim();
+                docs.Add(nomprenom);
+            }
+
+            foreach (string element in client)
+            {
+                listDoc.Items.Add(element);
+            }
+
+            reader.Close();
+        }
+
+        public void refreshLists()
+        {
+            client.Clear();
+            docs.Clear();
+            listClient.Items.Clear();
+            listDoc.Items.Clear();
+            ConsultClient();
+            ConsultDocs();
+        }
+    }
 }
--- PT4_Grp_2/Calendrier_modifier_rdv.cs
+++ PT4_Grp_2/Calendrier_modifier_rdv.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,39 @@ namespace PT4_Grp_2
 {
 	public partial class Calendrier_modifier_rdv : Modele_modifier
 	{
-		public Calendrier_modifier_rdv()
+        List<string> client = new List<string>();
+        static DB connexion = new DB("INFO-JOYEUX", "PT4_E2");
+        OleDbConnection dbCon = connexion.getDbConnection();
+        public Calendrier_modifier_rdv()
 		{
 			InitializeComponent();
 		}
-	}
+
+        public void ConsultClient()
+        {
+            client.Clear();
+            listClient.Items.Clear();
+            string sql = "select NOM,PRENOM from Personne " +
+                "left outer JOIN Personnel ON Personne.CODE_PERSONNE = Personnel.CODE_PERSONNEL " +
+                "WHERE Personnel.CODE_PERSONNEL is null ";
+
+
+            OleDbCommand cmdRead = new OleDbCommand(sql, dbCon);
+            OleDbDataReader reader = cmdRead.ExecuteReader();
+
+
+            while (reader.Read())
+            {
+                string nomprenom = reader.GetString(0).Trim() + reader.GetString(1).Trim();
+                client.Add(nomprenom);
+            }
+
+            foreach (string element in client)
+            {
+                listClient.Items.Add(element);
+            }
+
+            reader.Close();
+        }
+    }
 }
