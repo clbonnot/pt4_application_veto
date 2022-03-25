baa6973c4c5883b911b90d3af151fc775fda66e7
rbailhet <raphael.bailhet@etu.u-bordeaux.fr>
2022-03-25 16:58:40 +0100

    animaux

---
+++ PT4_Grp_2/Clients_Animal_add.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace PT4_Grp_2
+{
+	partial class Clients_Animal_add
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.progressBar2 = new System.Windows.Forms.ProgressBar();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.description = new System.Windows.Forms.Label();
+			this.name_medic = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.label7 = new System.Windows.Forms.Label();
+			this.add = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(87, 443);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(209, 20);
+			this.textBox4.TabIndex = 60;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(87, 264);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(209, 20);
+			this.textBox3.TabIndex = 59;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(87, 199);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(209, 20);
+			this.textBox2.TabIndex = 58;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(76, 95);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 57;
+			// 
+			// progressBar2
+			// 
+			this.progressBar2.BackColor = System.Drawing.Color.Honeydew;
+			this.progressBar2.ForeColor = System.Drawing.Color.DarkSeaGreen;
+			this.progressBar2.Location = new System.Drawing.Point(22, 43);
+			this.progressBar2.Name = "progressBar2";
+			this.progressBar2.Size = new System.Drawing.Size(336, 3);
+			this.progressBar2.TabIndex = 56;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(73, 9);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(223, 31);
+			this.label3.TabIndex = 55;
+			this.label3.Text = "Ajouter un animal";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(84, 244);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(41, 17);
+			this.label2.TabIndex = 54;
+			this.label2.Text = "Race";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(73, 132);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(97, 17);
+			this.label5.TabIndex = 53;
+			this.label5.Text = "Informations";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(84, 165);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 17);
+			this.label1.TabIndex = 52;
+			this.label1.Text = "Espèce";
+			// 
+			// description
+			// 
+			this.description.AutoSize = true;
+			this.description.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.description.Location = new System.Drawing.Point(84, 413);
+			this.description.Name = "description";
+			this.description.Size = new System.Drawing.Size(55, 17);
+			this.description.TabIndex = 51;
+			this.description.Text = "Allergie";
+			// 
+			// name_medic
+			// 
+			this.name_medic.AutoSize = true;
+			this.name_medic.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.name_medic.Location = new System.Drawing.Point(71, 65);
+			this.name_medic.Name = "name_medic";
+			this.name_medic.Size = new System.Drawing.Size(59, 26);
+			this.name_medic.TabIndex = 50;
+			this.name_medic.Text = "Nom";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(84, 298);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(45, 17);
+			this.label4.TabIndex = 61;
+			this.label4.Text = "Âge : ";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(135, 298);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(16, 17);
+			this.label6.TabIndex = 63;
+			this.label6.Text = "0";
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(87, 359);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker1.TabIndex = 64;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(84, 339);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(126, 17);
+			this.label7.TabIndex = 65;
+			this.label7.Text = "Date de naissance";
+			// 
+			// add
+			// 
+			this.add.BackColor = System.Drawing.Color.DarkSeaGreen;
+			this.add.Location = new System.Drawing.Point(250, 541);
+			this.add.Name = "add";
+			this.add.Size = new System.Drawing.Size(108, 36);
+			this.add.TabIndex = 68;
+			this.add.Text = "Ajouter";
+			this.add.UseVisualStyleBackColor = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(22, 541);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(108, 36);
+			this.button1.TabIndex = 67;
+			this.button1.Text = "Annuler";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.BackColor = System.Drawing.Color.Honeydew;
+			this.progressBar1.ForeColor = System.Drawing.Color.DarkSeaGreen;
+			this.progressBar1.Location = new System.Drawing.Point(22, 526);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(336, 3);
+			this.progressBar1.TabIndex = 66;
+			// 
+			// Clients_Animal_add
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Info;
+			this.ClientSize = new System.Drawing.Size(390, 589);
+			this.Controls.Add(this.add);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.progressBar2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.description);
+			this.Controls.Add(this.name_medic);
+			this.Name = "Clients_Animal_add";
+			this.Text = "Clients_Animal_add";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ProgressBar progressBar2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label description;
+		private System.Windows.Forms.Label name_medic;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button add;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+	}
+}
