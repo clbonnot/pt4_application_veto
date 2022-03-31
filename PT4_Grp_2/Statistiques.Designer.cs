22bd19fa92103396ef6b2b2272ee7ac1d2571e3d
marakotoaris <mathieu.rakotoarisoa@etu.u-bordeaux.fr>
2022-04-01 01:22:11 +0200

    top 3 ventes

--- PT4_Grp_2/Statistiques.Designer.cs
+++ PT4_Grp_2/Statistiques.Designer.cs
@@ -28,130 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-			this.label1 = new System.Windows.Forms.Label();
-			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.button4 = new System.Windows.Forms.Button();
-			this.button5 = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.BackColor = System.Drawing.Color.DarkGreen;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-			this.label1.Location = new System.Drawing.Point(324, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(156, 31);
-			this.label1.TabIndex = 67;
-			this.label1.Text = "Statistiques";
-			// 
-			// chart1
-			// 
-			this.chart1.BackColor = System.Drawing.Color.MediumAquamarine;
-			this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
-			this.chart1.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-			chartArea1.Name = "ChartArea1";
-			this.chart1.ChartAreas.Add(chartArea1);
-			legend1.Name = "Legend1";
-			this.chart1.Legends.Add(legend1);
-			this.chart1.Location = new System.Drawing.Point(142, 134);
-			this.chart1.Name = "chart1";
-			series1.ChartArea = "ChartArea1";
-			series1.Legend = "Legend1";
-			series1.Name = "Series1";
-			this.chart1.Series.Add(series1);
-			this.chart1.Size = new System.Drawing.Size(658, 316);
-			this.chart1.TabIndex = 68;
-			this.chart1.Text = "chart1";
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(206, 67);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(92, 47);
-			this.button1.TabIndex = 69;
-			this.button1.Text = "Top ventes";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(315, 67);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(92, 47);
-			this.button2.TabIndex = 70;
-			this.button2.Text = "Top Maladies";
-			this.button2.UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			this.button3.Location = new System.Drawing.Point(426, 67);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(92, 47);
-			this.button3.TabIndex = 71;
-			this.button3.Text = "Top Soins";
-			this.button3.UseVisualStyleBackColor = true;
-			// 
-			// button4
-			// 
-			this.button4.Location = new System.Drawing.Point(657, 67);
-			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(92, 47);
-			this.button4.TabIndex = 72;
-			this.button4.Text = "Top Animaux";
-			this.button4.UseVisualStyleBackColor = true;
-			// 
-			// button5
-			// 
-			this.button5.Location = new System.Drawing.Point(540, 67);
-			this.button5.Name = "button5";
-			this.button5.Size = new System.Drawing.Size(92, 47);
-			this.button5.TabIndex = 73;
-			this.button5.Text = "Top Produits";
-			this.button5.UseVisualStyleBackColor = true;
-			// 
-			// Statistiques
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.button5);
-			this.Controls.Add(this.button4);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.chart1);
-			this.Controls.Add(this.label1);
-			this.Name = "Statistiques";
-			this.Text = "Statistiques";
-			this.Controls.SetChildIndex(this.label1, 0);
-			this.Controls.SetChildIndex(this.chart1, 0);
-			this.Controls.SetChildIndex(this.button1, 0);
-			this.Controls.SetChildIndex(this.button2, 0);
-			this.Controls.SetChildIndex(this.button3, 0);
-			this.Controls.SetChildIndex(this.button4, 0);
-			this.Controls.SetChildIndex(this.button5, 0);
-			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.sales = new System.Windows.Forms.Button();
+            this.clients = new System.Windows.Forms.Button();
+            this.animals = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Deconnexion
+            // 
+            this.Deconnexion.FlatAppearance.BorderSize = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(324, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 31);
+            this.label1.TabIndex = 67;
+            this.label1.Text = "Statistiques";
+            // 
+            // chart
+            // 
+            this.chart.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.chart.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
+            this.chart.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(142, 134);
+            this.chart.Name = "chart";
+            this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            this.chart.Size = new System.Drawing.Size(658, 316);
+            this.chart.TabIndex = 68;
+            // 
+            // sales
+            // 
+            this.sales.Location = new System.Drawing.Point(206, 67);
+            this.sales.Name = "sales";
+            this.sales.Size = new System.Drawing.Size(92, 47);
+            this.sales.TabIndex = 69;
+            this.sales.Text = "Top ventes";
+            this.sales.UseVisualStyleBackColor = true;
+            this.sales.Click += new System.EventHandler(this.sales_Click);
+            // 
+            // clients
+            // 
+            this.clients.Location = new System.Drawing.Point(426, 67);
+            this.clients.Name = "clients";
+            this.clients.Size = new System.Drawing.Size(92, 47);
+            this.clients.TabIndex = 71;
+            this.clients.Text = "Top Clients";
+            this.clients.UseVisualStyleBackColor = true;
+            // 
+            // animals
+            // 
+            this.animals.Location = new System.Drawing.Point(657, 67);
+            this.animals.Name = "animals";
+            this.animals.Size = new System.Drawing.Size(92, 47);
+            this.animals.TabIndex = 72;
+            this.animals.Text = "Top Animaux";
+            this.animals.UseVisualStyleBackColor = true;
+            // 
+            // Statistiques
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.animals);
+            this.Controls.Add(this.clients);
+            this.Controls.Add(this.sales);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.label1);
+            this.Name = "Statistiques";
+            this.Text = "Statistiques";
+            this.Controls.SetChildIndex(this.Deconnexion, 0);
+            this.Controls.SetChildIndex(this.RoleName, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.chart, 0);
+            this.Controls.SetChildIndex(this.sales, 0);
+            this.Controls.SetChildIndex(this.clients, 0);
+            this.Controls.SetChildIndex(this.animals, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
 		#endregion
 
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Button button3;
-		private System.Windows.Forms.Button button4;
-		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+		private System.Windows.Forms.Button sales;
+		private System.Windows.Forms.Button clients;
+		private System.Windows.Forms.Button animals;
 	}
 }
--- PT4_Grp_2/Statistiques.cs
+++ PT4_Grp_2/Statistiques.cs
@@ -2,19 +2,53 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace PT4_Grp_2
 {
     public partial class Statistiques : Modele
     {
+        DB db;
         public Statistiques()
         {
             InitializeComponent();
+            this.db = new DB("info-joyeux", "PT4_E2");
+        }
+
+
+        private void sales_Click(object sender, EventArgs e)
+        {
+            chart.Series.Clear();
+            chart.Titles.Clear();
+            db.openConnection();
+            OleDbDataReader reader = db.select("select produit.nom, SUM(vente.quantite) from vente" +
+                " inner join produit on vente.code_produit = produit.code_produit " +
+                "group by produit.code_produit, produit.nom order by sum(vente.quantite) desc", null);
+            List<string> salesSeries = new List<string>();
+            List<int> salesValues = new List<int>();
+            while (reader.Read())
+            {
+                salesSeries.Add(reader.GetString(0));
+                salesValues.Add(reader.GetInt32(1));
+            }
+
+            chart.Titles.Add("Top 3 ventes");
+
+            string[] tabS = salesSeries.ToArray();
+            int[] tabValues = salesValues.ToArray();
+            for(int i = 0; i< tabS.Length; i++)
+            {
+                Series series = chart.Series.Add(tabS[i]);
+                series.Points.Add(tabValues[i]);
+            }
+            db.closeConnection();
+            chart.Refresh();
         }
     }
 }
