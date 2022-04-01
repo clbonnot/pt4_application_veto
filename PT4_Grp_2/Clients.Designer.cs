0441c585f5b53b0d1a562590e7c1a5b7256db15b
Aïssa KAHLOUL <aissageiffel@outlook.com>
2022-04-01 13:49:46 +0200

    Bouton delete client rajout

--- PT4_Grp_2/Clients.Designer.cs
+++ PT4_Grp_2/Clients.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.del = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listbox
@@ -38,18 +39,38 @@
             // 
             this.Deconnexion.FlatAppearance.BorderSize = 0;
             // 
+            // del
+            // 
+            this.del.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.del.Location = new System.Drawing.Point(15, 414);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(114, 24);
+            this.del.TabIndex = 26;
+            this.del.Text = "Supprimer";
+            this.del.UseVisualStyleBackColor = false;
+            this.del.Click += new System.EventHandler(this.Delete_Click);
+            // 
             // Clients
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.del);
             this.Name = "Clients";
             this.Text = "Clients";
+            this.Controls.SetChildIndex(this.RoleName, 0);
+            this.Controls.SetChildIndex(this.Deconnexion, 0);
+            this.Controls.SetChildIndex(this.listbox, 0);
+            this.Controls.SetChildIndex(this.add, 0);
+            this.Controls.SetChildIndex(this.detail, 0);
+            this.Controls.SetChildIndex(this.del, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button del;
     }
 }
