af2366b1f19fd311dd13bf2d87acf840bc4027d4
t.halley@numericable.fr <t.halley@numericable.fr>
2022-04-01 08:42:21 +0200

    classe medic

--- PT4_Grp_2/Soins.Designer.cs
+++ PT4_Grp_2/Soins.Designer.cs
@@ -28,46 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.Deconnexion = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// Deconnexion
-			// 
-			this.Deconnexion.FlatAppearance.BorderSize = 0;
-			this.Deconnexion.Location = new System.Drawing.Point(692, 12);
-			this.Deconnexion.Name = "Deconnexion";
-			this.Deconnexion.Size = new System.Drawing.Size(81, 24);
-			this.Deconnexion.TabIndex = 10;
-			this.Deconnexion.Text = "Deconnexion";
-			this.Deconnexion.UseVisualStyleBackColor = true;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.BackColor = System.Drawing.Color.DarkGreen;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-			this.label1.Location = new System.Drawing.Point(340, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(82, 31);
-			this.label1.TabIndex = 67;
-			this.label1.Text = "Soins";
-			// 
-			// Soins
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.Deconnexion);
-			this.Name = "Soins";
-			this.Text = "Soins";
-			this.Controls.SetChildIndex(this.Deconnexion, 0);
-			this.Controls.SetChildIndex(this.label1, 0);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.Deconnexion = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listMedic = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Deconnexion
+            // 
+            this.Deconnexion.FlatAppearance.BorderSize = 0;
+            this.Deconnexion.Location = new System.Drawing.Point(692, 12);
+            this.Deconnexion.Name = "Deconnexion";
+            this.Deconnexion.Size = new System.Drawing.Size(81, 24);
+            this.Deconnexion.TabIndex = 10;
+            this.Deconnexion.Text = "Deconnexion";
+            this.Deconnexion.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(340, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 31);
+            this.label1.TabIndex = 67;
+            this.label1.Text = "Soins";
+            // 
+            // listMedic
+            // 
+            this.listMedic.FormattingEnabled = true;
+            this.listMedic.Location = new System.Drawing.Point(167, 70);
+            this.listMedic.Name = "listMedic";
+            this.listMedic.Size = new System.Drawing.Size(621, 277);
+            this.listMedic.TabIndex = 68;
+            // 
+            // Soins
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listMedic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Deconnexion);
+            this.Name = "Soins";
+            this.Text = "Soins";
+            this.Controls.SetChildIndex(this.Deconnexion, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.listMedic, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,5 +86,6 @@
 
 		private System.Windows.Forms.Button Deconnexion;
 		private System.Windows.Forms.Label label1;
-	}
+        private System.Windows.Forms.ListBox listMedic;
+    }
 }
--- PT4_Grp_2/Soins.cs
+++ PT4_Grp_2/Soins.cs
@@ -12,13 +12,19 @@ namespace PT4_Grp_2
 {
     public partial class Soins : Modele_listbox
     {
+        List<Product> allProduct;
+        DB db;
+
         Soins_add SA;
         public Soins()
         {
             InitializeComponent();
+            this.db = new DB("info-joyeux", "PT4_E2");
+            makeListBox();
+
         }
 
-		private void progressBar2_Click(object sender, EventArgs e)
+        private void progressBar2_Click(object sender, EventArgs e)
 		{
 
 		}
@@ -28,5 +34,36 @@ namespace PT4_Grp_2
             SA = new Soins_add();
             
         }
+
+        /**
+         * Function that fills the list of products with all the product in the database ordered by their name.
+         */
+        private void makeAllProduct()
+        {
+            allProduct = new List<Product>();
+            db.openConnection();
+            OleDbDataReader reader = db.select("select code_produit from produit order by nom asc", null);
+            while (reader.Read())
+            {
+                allProduct.Add(new Product(reader.GetInt32(0), db));
+            }
+            db.closeConnection();
+
+        }
+
+        /**
+       * Function that fills the listbox with all the product 
+       */
+        private void makeListBox()
+        {
+            listbox.Items.Clear();
+            makeAllProduct();
+            foreach (Product p in allProduct)
+            {
+                listbox.Items.Add(p.ToString());
+            }
+            listbox.Refresh();
+        }
+
     }
 }
